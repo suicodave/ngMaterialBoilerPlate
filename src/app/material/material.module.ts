a0c00f0661e8156d1eb56e06be0ed435d797ffcd
suicodave <suico.dave@gmail.com>
2018-11-08 13:29:19 +0800

    feat(app) basic material structure

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material';
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatCardModule
+  ],
+  exports: [
+    MatCardModule
+  ]
+})
+export class MaterialModule { }
